5a6de973a8e94eeef43404de7e45e0e8d4e1b359
Jhanoo <jhanoo@naver.com>
2024-04-18 18:44:08 +0900

    tmp

--- index.js
+++ index.js
@@ -1,71 +1,30 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const { MongoClient, ServerApiVersion } = require('mongodb')
 const path = require('path')
-
-// mongodb+srv://jhanoo15:hanoo1541@runnershy-cluster.fxsr8l8.mongodb.net/
-
-// MongoDB 연결 정보
-const uri = 'mongodb+srv://jhanoo15:hanoo1541@runnershy-cluster.fxsr8l8.mongodb.net/'
-const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    },
-})
 
 // body-parser 미들웨어 설정
 // app.use(bodyParser.urlencoded({ extended: true }))
 
 // 정적 파일 제공
-app.use(express.static(path.join(__dirname, 'public')))
+app.use('/', express.static(path.join(__dirname, 'public')))
 
-// 루트 경로에 대한 GET 요청 처리
+app.use('/signup', express.static(path.join(__dirname, 'public/signup')))
+
+app.use('/users', express.static(path.join(__dirname, 'public/users')))
+
+// 루트 경로 => index.html
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+    res.sendFile(path.join(__dirname, 'index.html'))
 })
 
-// 루트 경로에 대한 GET 요청 처리
 app.get('/signup', (req, res) => {
     res.sendFile(__dirname + '/signup.html')
 })
 
-app.get('/users', (req, res) => {
-    res.sendFile(__dirname + '/users.html')
-})
+// app.post('/users', async (req, res) => {
 
-app.post('/users', async (req, res) => {
-    try {
-        await client.connect() // MongoDB에 연결
-        const db = client.db('mydatabase') // 데이터베이스 선택
-        const collection = db.collection('users') // 컬렉션 선택
-
-        // 모든 사용자 정보 가져오기
-        const users = await collection.find({}).toArray()
-
-        // HTML 형식으로 지원자 정보를 표시
-        let html = '<h1>지원자 정보</h1>'
-        html += '<table border="1">'
-        html += '<tr><th>이름</th><th>학교</th><th>학과</th><th>학번</th><th>전화번호</th><th>성별</th><th>재학여부</th></tr>'
-        users.forEach((user) => {
-            html += `<tr><td>${user.name}</td><td>${user.university}</td><td>${user.major}</td><td>${user.studentNum}</td><td>${user.phoneNum}</td><td>${user.gender}</td><td>${user.enroll}</td></tr>`
-        })
-        html += '</table>'
-
-        // 클라이언트에게 HTML 응답 전송
-        res.send(html)
-
-        // 가져온 사용자 정보 출력
-        // res.json(users)
-    } catch (error) {
-        console.error('사용자 정보를 가져오는 중 오류:', error)
-        res.status(500).send('사용자 정보를 가져오는 중 오류가 발생했습니다.')
-    } finally {
-        await client.close() // MongoDB 연결 종료
-    }
-})
+// })
 
 // 회원가입 폼 제출 처리
 app.post('/signup', async (req, res) => {
@@ -107,7 +66,7 @@ app.post('/signup', async (req, res) => {
     }
 })
 
-// 회원가입 완료 페이지 라우트
+// 회원가입 완료 페이지
 app.get('/signup-success', (req, res) => {
     res.send('지원서 작성이 완료되었습니다!')
 })
